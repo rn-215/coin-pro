d12670c980924f0e635110c488aabfa6739a700d
Carlton Tucker <carltontucker52@gmail.com>
2021-11-08 12:59:55 +0100

    profile screen

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState}from 'react'
+import { View, Text, Image, Pressable } from 'react-native'
+import styles from './styles'
+
+const image = require('../../../assets/images/Saly-16.png')
 
 const ProfileScreen = () => {
+    const [user, setUser] = useState({
+        id: '1',
+        name: 'Ernest',
+        email: 'ernestnamdi6@gmail.com',
+        image: 'https://s2.coinmarketcap.com/static/img/coins/200x200/1839.png',
+        netWorth: 34567
+    })
+
+    const signout = () => {
+        console.warn('signout')
+    }
+    
     return (
-        <View style={{flex: 1, justifyContent:"center", alignItems:"center"}}>
-            <Text>profile component</Text>
+        <View style={styles.root}>
+            <Image style={styles.image} source={image} />
+
+
+            <View style={styles.userContainer}>
+                <View style={{flexDirection:"row", alignItems:"center"}}>
+                <Image style={styles.images} source={{uri: user.image}}/>
+                
+                    <View style={styles.second}>
+                        <Text style={styles.name}>{user.name}</Text>
+                        <Text style={styles.email}>{user.email}</Text>  
+                    </View>
+                   
+                </View>
+
+
+                <View>
+                    <Text style={{ fontSize: 18, color:"white"}}>{user.netWorth}</Text>
+                </View>
+            
+        </View>
+
+        <Pressable  onPress={signout} style={{marginTop:"auto", alignSelf:"center"}}>
+            <Text style={{color: 'white'}}>Sign Out</Text>
+        </Pressable>
         </View>
     )
 }
--- src/screens/ProfileScreen/styles.ts
+++ src/screens/ProfileScreen/styles.ts
@@ -1,6 +1,48 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        backgroundColor:"black",
+        padding: 20,
+        
+    },
+    image: {
+        height: 150,
+        resizeMode:"contain",
+        alignSelf:"center",
+        marginTop:20,
+       
+
+    },
+    name: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "white",
+        marginTop: 20,
+        marginBottom: 10,
+       
+    },
+    email: {
+        fontSize: 16,
+        color: "white",
+        marginBottom: 20,
+       
+    },
+    images:{
+        height: 50,
+        width: 50,
+        resizeMode:"contain",
+    },
+    userContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "100%",
+    },
+    second:{
+        marginLeft: 10
+    }
 
 })
 
